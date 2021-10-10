8a8e351d675d4066e516fd8907cb833b2f4e2bf4
sruthivangara <91916674+sruthivangara@users.noreply.github.com>
2021-10-10 18:16:38 -0500

    Add files via upload

---
+++ SruthiV.py
@@ -0,0 +1,30 @@
+
+first_name = "sruthi" #1.created a variable first_name to store first name
+last_name = "VANGARA" #2.created a variable last_name to store last name
+print("Hello",first_name.upper(),last_name.lower()) #3.using string function upper() to convert first_name into upper case and function lower() to convert last_name into lower case
+print() #4. This will print new line
+print()  #This will print new line
+full_name = ("sruthi  vangara") #5. created a variable full_name to store both first and last name
+sliced_text = slice(6) #using slice() function
+print(full_name[sliced_text]) #6.prints the sliced text and prints in new line
+x = full_name.replace("vangara","Walsh College Student") # 7.uses replace function to replace the last name that stored in full_name
+print(x) #prints the output of variable x
+print('"Start by doing what\'s necessary; then do what\'s possible; and suddenly you are doing the impossible - Francis of Assisi"') #8.it prints the given statement in question with double quotes
+
+var1 = 10 #9.stores a var1
+var2 = 5 #stores a var2
+addition = var1 + var2  #10.formula for addition
+subtraction = var1 - var2 #formula for subtraction
+multiplication = var1 * var2 #formula for multiplication
+division = var1 / var2 #formula for division
+print('numeric value of variable',var1,'plus','numeric value of variable',var2,'equals',addition) #11.will print the addition of var1 and var2
+print('numeric value of variable',var1,'minus','numeric value of variable',var2,'equals',subtraction) #will print the subtraction of var1 and var2
+print('numeric value of variable',var1,'multiplied by','numeric value of variable',var2,'equals',multiplication) #will print the multiplication of var1 and var2
+print('numeric value of variable',var1,'divided by','numeric value of variable',var2,'equals',division) #will print the division of var1 and var2
+sq_root =  multiplication ** 0.5 #12.square root is taken from the multiplication number and its formula
+print('value of square root that stored the result of multiplication',multiplication,'equals',sq_root) #It prints the sqare root
+current_month = "october" #13.created a variable as current_month as string
+day_of_current_month = 8 #created a variable to take day of current month
+print("Today is day", day_of_current_month,"\t\tof month",current_month) #14.it prints the output with tabbed over two times
+
+#GitHub Repository link https://github.com/sruthivangara/SruthiV
